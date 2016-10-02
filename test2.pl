9bfeed39103b7b878de040e05ac67e4f91445f66
jcalebc55 <jcalebc55@gmail.com>
2016-10-02 17:51:15 -0500

    modified:   test2.pl    Height of tree.

--- test2.pl
+++ test2.pl
@@ -8,22 +8,52 @@ append([X|Y],Z,[X|W]) :- append(Y,Z,W).    % your first line
 
 
 
-%smallest(X,Y,Z,D):- Y==nil,D is X.  
+% smallest(X,Y,Z,D):- Y==nil,D is X.  
 
-%smallest(X,Y,Z,D):- Y\=nil, D is Y.
+% smallest(X,Y,Z,D):- Y\=nil, D is Y.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% smallest
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 smallest(S,tree(X,L,R)) :- L==nil, S is X.
 
 smallest(S,tree(X,L,R)) :- L\=nil, not(number(L)), smallest(S,L).
 
 smallest(S,tree(X,L,R)) :- L\= nil, number(L), S is L.
-
-
+%%%%%%%%%%%%%
+%Member
+%%%%%%%%%%%%
 member(M, tree(X,L,R) ):- M==X, M is X.
 
 member(M,tree(X,L,R)):- X > M, member(M,L).
 
 member(M,tree(X,L,R)) :- X< M, member(M, R).
+
+%%%%%%%%%%%%%%%
+% height
+%%%%%%%%%%%
+max(H,L,R):- L > R, H is L.
+
+max(H,L,R):- L <R, H is R.
+
+max(H,L,R):- L==R, H is L.
+
+
+height(nil, H) :- H is 0.
+
+height(tree(X,L,R), H):- height(L,LH), height(R,RH), max(Z,LH,RH), H is 1 +Z.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
