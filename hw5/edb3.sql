0ae2d4436573df1ed70478c7270470cca09df639
jcalebc55 <jcalebc55@gmail.com>
2016-11-21 00:30:38 -0600

    new file:   edb1.sql 	new file:   edb1inert 	new file:   edb2.sql 	new file:   edb2insert 	new file:   edb3.sql 	new file:   edb3insert          new file:   hw5 	new file:   java_print

---
+++ hw5/edb3.sql
@@ -0,0 +1,376 @@
+insert into parent values('A0','P0');
+insert into parent values('B0','P0');
+insert into parent values('C0','P0');
+--
+insert into parent values('A00','A0');
+insert into parent values('A01','A0');
+insert into parent values('A02','A0');
+--
+insert into parent values('A000','A00');
+insert into parent values('A001','A00');
+insert into parent values('A002','A00');
+--
+insert into parent values('A010','A01');
+insert into parent values('A011','A01');
+insert into parent values('A012','A01');
+--
+insert into parent values('A020','A02');
+insert into parent values('A021','A02');
+insert into parent values('A022','A02');
+--
+insert into parent values('A030','A03');
+insert into parent values('A031','A03');
+insert into parent values('A032','A03');
+--
+insert into parent values('A040','A04');
+insert into parent values('A041','A04');
+insert into parent values('A042','A04');
+--
+insert into parent values('A0000','A000');
+insert into parent values('A0001','A000');
+insert into parent values('A0002','A000');
+--
+insert into parent values('A0010','A001');
+insert into parent values('A0011','A001');
+insert into parent values('A0012','A001');
+--
+insert into parent values('A0020','A002');
+insert into parent values('A0021','A002');
+insert into parent values('A0022','A002');
+-
+insert into parent values('A0030','A003');
+insert into parent values('A0031','A003');
+insert into parent values('A0032','A003');
+--
+insert into parent values('A0040','A004');
+insert into parent values('A0041','A004');
+insert into parent values('A0042','A004');
+--
+insert into parent values('A0100','A010');
+insert into parent values('A0101','A010');
+insert into parent values('A0102','A010');
+--
+insert into parent values('A0110','A011');
+insert into parent values('A0111','A011');
+insert into parent values('A0112','A011');
+--
+insert into parent values('A0120','A012');
+insert into parent values('A0121','A012');
+insert into parent values('A0122','A012');
+--
+insert into parent values('A0130','A013');
+insert into parent values('A0131','A013');
+insert into parent values('A0132','A013');
+--
+insert into parent values('A0140','A014');
+insert into parent values('A0141','A014');
+insert into parent values('A0142','A014');
+--
+insert into parent values('A0200','A020');
+insert into parent values('A0201','A020');
+insert into parent values('A0202','A020');
+--
+insert into parent values('A0210','A021');
+insert into parent values('A0211','A021');
+insert into parent values('A0212','A021');
+--
+insert into parent values('A0220','A022');
+insert into parent values('A0221','A022');
+insert into parent values('A0222','A022');
+--
+insert into parent values('A0230','A023');
+insert into parent values('A0231','A023');
+insert into parent values('A0232','A023');
+--
+insert into parent values('A0240','A024');
+insert into parent values('A0241','A024');
+insert into parent values('A0242','A024');
+--
+insert into parent values('A0300','A030');
+insert into parent values('A0301','A030');
+insert into parent values('A0302','A030');
+--
+insert into parent values('A0310','A031');
+insert into parent values('A0311','A031');
+insert into parent values('A0312','A031');
+--
+insert into parent values('A0320','A032');
+insert into parent values('A0321','A032');
+insert into parent values('A0322','A032');
+--
+insert into parent values('A0330','A033');
+insert into parent values('A0331','A033');
+insert into parent values('A0332','A033');
+--
+insert into parent values('A0340','A034');
+insert into parent values('A0341','A034');
+insert into parent values('A0342','A034');
+--
+insert into parent values('A0400','A040');
+insert into parent values('A0401','A040');
+insert into parent values('A0402','A040');
+--
+insert into parent values('A0410','A041');
+insert into parent values('A0411','A041');
+insert into parent values('A0412','A041');
+--
+insert into parent values('A0420','A042');
+insert into parent values('A0421','A042');
+insert into parent values('A0422','A042');
+--
+insert into parent values('A0430','A043');
+insert into parent values('A0431','A043');
+insert into parent values('A0432','A043');
+--
+insert into parent values('A0440','A044');
+insert into parent values('A0441','A044');
+insert into parent values('A0442','A044');
+--
+insert into parent values('B00','B0');
+insert into parent values('B01','B0');
+insert into parent values('B02','B0');
+--
+insert into parent values('B000','B00');
+insert into parent values('B001','B00');
+insert into parent values('B002','B00');
+--
+insert into parent values('B010','B01');
+insert into parent values('B011','B01');
+insert into parent values('B012','B01');
+--
+insert into parent values('B020','B02');
+insert into parent values('B021','B02');
+insert into parent values('B022','B02');
+--
+insert into parent values('B030','B03');
+insert into parent values('B031','B03');
+insert into parent values('B032','B03');
+--
+insert into parent values('B040','B04');
+insert into parent values('B041','B04');
+insert into parent values('B042','B04');
+--
+insert into parent values('B0000','B000');
+insert into parent values('B0001','B000');
+insert into parent values('B0002','B000');
+--
+insert into parent values('B0010','B001');
+insert into parent values('B0011','B001');
+insert into parent values('B0012','B001');
+--
+insert into parent values('B0020','B002');
+insert into parent values('B0021','B002');
+insert into parent values('B0022','B002');
+-
+insert into parent values('B0030','B003');
+insert into parent values('B0031','B003');
+insert into parent values('B0032','B003');
+--
+insert into parent values('B0040','B004');
+insert into parent values('B0041','B004');
+insert into parent values('B0042','B004');
+--
+insert into parent values('B0100','B010');
+insert into parent values('B0101','B010');
+insert into parent values('B0102','B010');
+--
+insert into parent values('B0110','B011');
+insert into parent values('B0111','B011');
+insert into parent values('B0112','B011');
+--
+insert into parent values('B0120','B012');
+insert into parent values('B0121','B012');
+insert into parent values('B0122','B012');
+--
+insert into parent values('B0130','B013');
+insert into parent values('B0131','B013');
+insert into parent values('B0132','B013');
+--
+insert into parent values('B0140','B014');
+insert into parent values('B0141','B014');
+insert into parent values('B0142','B014');
+--
+insert into parent values('B0200','B020');
+insert into parent values('B0201','B020');
+insert into parent values('B0202','B020');
+--
+insert into parent values('B0210','B021');
+insert into parent values('B0211','B021');
+insert into parent values('B0212','B021');
+--
+insert into parent values('B0220','B022');
+insert into parent values('B0221','B022');
+insert into parent values('B0222','B022');
+--
+insert into parent values('B0230','B023');
+insert into parent values('B0231','B023');
+insert into parent values('B0232','B023');
+--
+insert into parent values('B0240','B024');
+insert into parent values('B0241','B024');
+insert into parent values('B0242','B024');
+--
+insert into parent values('B0300','B030');
+insert into parent values('B0301','B030');
+insert into parent values('B0302','B030');
+--
+insert into parent values('B0310','B031');
+insert into parent values('B0311','B031');
+insert into parent values('B0312','B031');
+--
+insert into parent values('B0320','B032');
+insert into parent values('B0321','B032');
+insert into parent values('B0322','B032');
+--
+insert into parent values('B0330','B033');
+insert into parent values('B0331','B033');
+insert into parent values('B0332','B033');
+--
+insert into parent values('B0340','B034');
+insert into parent values('B0341','B034');
+insert into parent values('B0342','B034');
+--
+insert into parent values('B0400','B040');
+insert into parent values('B0401','B040');
+insert into parent values('B0402','B040');
+--
+insert into parent values('B0410','B041');
+insert into parent values('B0411','B041');
+insert into parent values('B0412','B041');
+--
+insert into parent values('B0420','B042');
+insert into parent values('B0421','B042');
+insert into parent values('B0422','B042');
+--
+insert into parent values('B0430','B043');
+insert into parent values('B0431','B043');
+insert into parent values('B0432','B043');
+--
+insert into parent values('B0440','B044');
+insert into parent values('B0441','B044');
+insert into parent values('B0442','B044');
+--
+insert into parent values('C00','C0');
+insert into parent values('C01','C0');
+insert into parent values('tom','C0');
+--
+insert into parent values('C000','C00');
+insert into parent values('C001','C00');
+insert into parent values('C002','C00');
+--
+insert into parent values('C010','C01');
+insert into parent values('C011','C01');
+insert into parent values('C012','C01');
+--
+insert into parent values('C020','tom');
+insert into parent values('C021','tom');
+insert into parent values('C022','tom');
+--
+insert into parent values('C030','C03');
+insert into parent values('C031','C03');
+insert into parent values('C032','C03');
+--
+insert into parent values('C040','C04');
+insert into parent values('C041','C04');
+insert into parent values('C042','C04');
+--
+insert into parent values('C0000','C000');
+insert into parent values('C0001','C000');
+insert into parent values('C0002','C000');
+--
+insert into parent values('C0010','C001');
+insert into parent values('C0011','C001');
+insert into parent values('C0012','C001');
+--
+insert into parent values('C0020','C002');
+insert into parent values('C0021','C002');
+insert into parent values('C0022','C002');
+-
+insert into parent values('C0030','C003');
+insert into parent values('C0031','C003');
+insert into parent values('C0032','C003');
+--
+insert into parent values('C0040','C004');
+insert into parent values('C0041','C004');
+insert into parent values('C0042','C004');
+--
+insert into parent values('C0100','C010');
+insert into parent values('C0101','C010');
+insert into parent values('C0102','C010');
+--
+insert into parent values('C0110','C011');
+insert into parent values('C0111','C011');
+insert into parent values('C0112','C011');
+--
+insert into parent values('C0120','C012');
+insert into parent values('C0121','C012');
+insert into parent values('C0122','C012');
+--
+insert into parent values('C0130','C013');
+insert into parent values('C0131','C013');
+insert into parent values('C0132','C013');
+--
+insert into parent values('C0140','C014');
+insert into parent values('C0141','C014');
+insert into parent values('C0142','C014');
+--
+insert into parent values('C0200','C020');
+insert into parent values('C0201','C020');
+insert into parent values('C0202','C020');
+--
+insert into parent values('C0210','C021');
+insert into parent values('C0211','C021');
+insert into parent values('C0212','C021');
+--
+insert into parent values('C0220','C022');
+insert into parent values('C0221','C022');
+insert into parent values('C0222','C022');
+--
+insert into parent values('C0230','C023');
+insert into parent values('C0231','C023');
+insert into parent values('C0232','C023');
+--
+insert into parent values('C0240','C024');
+insert into parent values('C0241','C024');
+insert into parent values('C0242','C024');
+--
+insert into parent values('C0300','C030');
+insert into parent values('C0301','C030');
+insert into parent values('C0302','C030');
+--
+insert into parent values('C0310','C031');
+insert into parent values('C0311','C031');
+insert into parent values('C0312','C031');
+--
+insert into parent values('C0320','C032');
+insert into parent values('C0321','C032');
+insert into parent values('C0322','C032');
+--
+insert into parent values('C0330','C033');
+insert into parent values('C0331','C033');
+insert into parent values('C0332','C033');
+--
+insert into parent values('C0340','C034');
+insert into parent values('C0341','C034');
+insert into parent values('C0342','C034');
+--
+insert into parent values('C0400','C040');
+insert into parent values('C0401','C040');
+insert into parent values('C0402','C040');
+--
+insert into parent values('C0410','C041');
+insert into parent values('C0411','C041');
+insert into parent values('C0412','C041');
+--
+insert into parent values('C0420','C042');
+insert into parent values('C0421','C042');
+insert into parent values('C0422','C042');
+--
+insert into parent values('C0430','C043');
+insert into parent values('C0431','C043');
+insert into parent values('C0432','C043');
+--
+insert into parent values('C0440','C044');
+insert into parent values('C0441','C044');
+insert into parent values('C0442','C044');
+--
